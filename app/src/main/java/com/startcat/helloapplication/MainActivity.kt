f4c2cbf77bbfd4c5b856ee9bc09d8683b1eb9961
zhaihai <duke_zh@yeah.net>
2021-09-25 15:23:23 +0800

    init

--- app/src/main/java/com/startcat/helloapplication/MainActivity.kt
+++ app/src/main/java/com/startcat/helloapplication/MainActivity.kt
@@ -2,12 +2,11 @@ package com.startcat.helloapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.startcat.mylibrary.Tools
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Tools.Name
     }
 }
